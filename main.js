6f5052c191dbea3e0dfb917e6995d8ce5af35d26
lytik22 <levelplusbylytik@gmail.com>
2025-03-30 13:55:51 +0300

    add mobile support

--- main.js
+++ main.js
@@ -9,24 +9,6 @@ const best = document.querySelector(".best");
 const home = document.querySelector(".h");
 const best2 = document.querySelector(".b");
 const join2 = document.querySelector(".j");
-best2.addEventListener("click",()=>{ 
-    best.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
-});
-home.addEventListener("click",()=>{
-    bunner.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    })
-})
-join2.addEventListener("click",()=>{
-    join.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    })
-})
 
 btn2.addEventListener("click",()=>{
     window.location.href = "https://go.mo.co/invite/be6f81a9-7246-4884-942b-d6a2e8ccbdbf"
